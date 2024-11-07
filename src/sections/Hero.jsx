50cd56f2054093fec6637cccee5aa3e879e5f843
micky <micky.companie@gmail.com>
2024-11-07 17:06:08 +0100

    HeroSection: placing the 3D model + lights

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,17 @@
+import {Canvas} from "@react-three/fiber";
+import {PerspectiveCamera} from "@react-three/drei";
+import {Suspense} from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import OldComputers from "../components/OldComputers.jsx";
+import {useMediaQuery} from "react-responsive";
+// import {calculateSizes} from "../constants/index.js";
 
 const Hero = () => {
+    // const isSmall = useMediaQuery({maxWidth: 440})
+    const isMobile = useMediaQuery({maxWidth: 768})
+    // const isTablet = useMediaQuery({minWidth: 769, maxWidth: 1024})
+    // const sizes = calculateSizes(isSmall, isMobile, isTablet)
+
     return (
         <section className="min-h-screen w-full flex flex-col relative">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -12,7 +24,30 @@ const Hero = () => {
             </div>
 
             <div className="w-full h-full absolute inset-0">
-
+                <Canvas className="w-full h-full">
+                    <Suspense fallback={<CanvasLoader/>}>
+                        <PerspectiveCamera
+                            makeDefault
+                            position={[0, 0, 30]}
+                        />
+                        <OldComputers
+                            scale={isMobile ? 1.6 : 2.3}
+                            position={[0, -10.1, -1.6]}
+                            rotation={[-6.2, 0, 0]}
+                        />
+                        <ambientLight
+                            intensity={0.3}
+                            position={[0, 0, 0]}
+                            color={"#F5E2D8FF"}
+                        />
+                        <directionalLight
+                            position={[10, 10, 10]}
+                            rotation={[0, 0, 33]}
+                            intensity={2}
+                            color={"#15ABFF"}
+                        />
+                    </Suspense>
+                </Canvas>
             </div>
         </section>
     )
